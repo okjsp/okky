12290edb529d6727088d95ce78c3922e199d699b
정병태 <try9810@gmail.com>
2015-03-15 06:27:52 +0900

    홍보 갯수 조절

--- grails-app/controllers/net/okjsp/MainController.groovy
+++ grails-app/controllers/net/okjsp/MainController.groovy
@@ -21,7 +21,7 @@ class MainController {
 
 //        promoteArticles = promoteArticles.unique { a, b -> a.createIp <=> b.createIp }
 
-        if(promoteArticles?.size() > 3) promoteArticles = promoteArticles.subList(0, 10)
+        if(promoteArticles?.size() > 3) promoteArticles = promoteArticles.subList(0, 5)
         
         return [
             isIndex: true,
