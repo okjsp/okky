012756d35f40074c6c10a6d83b4b7f5bf70a45ff
langerhans <try9810@gmail.com>
2016-08-23 20:37:06 +0900

    베너 클릭 카운트 쿼리 추가

--- grails-app/domain/net/okjsp/Banner.groovy
+++ grails-app/domain/net/okjsp/Banner.groovy
@@ -7,17 +7,37 @@ class Banner {
     String image
     String url
     String target
-    int clickCount = 0
     boolean visible = true
 
     Date dateCreated
     Date lastUpdated
 
+    int clickCount = 0
+    int ipCount = 0
+
+    static transients = ['clickCount', 'ipCount']
+
     static constraints = {
         target nullable: true
+        image nullable: true
     }
 
     static mapping = {
         cache true
+    }
+
+    Integer getIpCount() {
+        BannerClick.where {
+            banner == this
+        }.count()
+    }
+
+    Integer getClickCount() {
+        BannerClick.createCriteria().get {
+            projections {
+                sum('clickCount')
+            }
+            eq('banner', this)
+        }
     }
 }
